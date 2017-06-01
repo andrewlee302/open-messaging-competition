663197ae4599b5568f1f922a5be3462891054b8e
Andrew Lee <lichundian@gmail.com>
2017-06-01 23:49:50 +0800

    tune

--- src/main/java/io/openmessaging/demo/Config.java
+++ src/main/java/io/openmessaging/demo/Config.java
@@ -20,7 +20,7 @@ public class Config {
 	/**
 	 *  it directly relates to the messages' lifecycle.
 	 */
-	public static final int SEGMENT_SIZE = 1 << 18;
+	public static final int SEGMENT_SIZE = 1 << 14;
 
 	public static final int AVERAGE_MSG_SIZE = 157;
 
