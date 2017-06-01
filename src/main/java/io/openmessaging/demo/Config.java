890129783bd66d14c53abeb6e1b1983d37816890
Andrew Lee <lichundian@gmail.com>
2017-06-01 23:49:06 +0800

    tune

--- src/main/java/io/openmessaging/demo/Config.java
+++ src/main/java/io/openmessaging/demo/Config.java
@@ -16,18 +16,16 @@ public class Config {
 	public static final int WRITE_REQUEST_QUEUE_SIZE = 32;
 	public static final int REQ_BATCH_COUNT_THRESHOLD = 64;
 	public static final long REQ_WAIT_TIME_THRESHOLD = 300; // 500ms
-	public static final int WRITE_SEGMENT_QUEUE_SIZE = 10;
+	public static final int WRITE_SEGMENT_QUEUE_SIZE = 20;
 	/**
 	 *  it directly relates to the messages' lifecycle.
 	 */
-	public static final int SEGMENT_SIZE = 1 << 17;
+	public static final int SEGMENT_SIZE = 1 << 18;
 
-	public static final int AVERAGE_MSG_SIZE = 100;
+	public static final int AVERAGE_MSG_SIZE = 157;
 
 
-	public static final int DEFAULT_KEYVALUE_MAP_SIZE = 18;
-
-	public static final int NUM_ENCODER_MESSAGE_THREAD = 4;
+	public static final int NUM_ENCODER_MESSAGE_THREAD = 8;
 	public static final int READ_BUFFER_QUEUE_SIZE = 1000; // very large
 	public static final int READ_MSG_QUEUE_SIZE = Integer.MAX_VALUE;
 
